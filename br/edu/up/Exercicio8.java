c51a9ab0840565ee77627dd0cfa68759a5e56bda
GustavoCirino2802 <142018650+GustavoCirino2802@users.noreply.github.com>
2024-04-22 23:55:52 -0300

    Exercicio8

--- br/edu/up/Exercicio8.java
+++ br/edu/up/Exercicio8.java
@@ -1,5 +1,32 @@
 package br.edu.up;
+import br.edu.up.modelos.Aluno;
 
 public class Exercicio8 {
 
+    public static void executar(){
+        String nome = Prompt.lerLinha("Digite o seu nome: ");
+        Double[] notas = new Double[3];
+
+        for (int i = 0; i < 3; i++) {
+            notas[i] = Prompt.lerDecimal("Digite a nota " + (i + 1)  + ": ");
+        }
+
+        Aluno aluno = new Aluno(nome, notas);
+
+        Prompt.separador();
+        Prompt.imprimir("Nome do aluno: " + aluno.getnome());
+        Prompt.imprimir("Nota Final: " + aluno.getnotaFinal());
+        if (aluno.getnotaFinal() <= 5) {
+            Prompt.imprimir("REPROVADO!");
+        }
+        else if (aluno.getnotaFinal() >=5.1 && aluno.getnotaFinal() <= 6.9) {
+            Prompt.imprimir("RECUPERAÇÃO!");
+        }
+        else if (aluno.getnotaFinal() >= 7) {
+            Prompt.imprimir("APROVADO!");
+            
+        }     
+        Prompt.separador();
+    
+    }
 }
--- br/edu/up/Programa.java
+++ br/edu/up/Programa.java
@@ -9,7 +9,7 @@ public class Programa {
         //Exercicio5.executar();
         //Exercicio6.executar();
         //Exercicio7.executar();
-        //Exercicio8.executar();
+        Exercicio8.executar();
         //Exercicio9.executar();
         //Exercicio10.executar();
         //Exercicio11.executar();
