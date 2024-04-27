9aa020db0848ddc1de1fbf8b94487bfaa36847c7
GustavoCirino2802 <142018650+GustavoCirino2802@users.noreply.github.com>
2024-04-27 20:35:58 -0300

    Exercicio14

--- br/edu/up/Exercicio14.java
+++ br/edu/up/Exercicio14.java
@@ -1,5 +1,30 @@
 package br.edu.up;
+import br.edu.up.modelos.Produto;
+
 
 public class Exercicio14 {
+    public static void executar() {
+        int numProdutos = 40;
+        double totalPrecoCusto = 0;
+        double totalPrecoVenda = 0;
 
+        for (int i = 0; i < numProdutos; i++) {
+            double precoCusto = Prompt.lerDecimal("Digite o preço de custo do produto " + (i + 1) + ":");
+            double precoVenda = Prompt.lerDecimal("Digite o preço de venda do produto " + (i + 1) + ":");
+
+            Produto produto = new Produto(precoCusto, precoVenda);
+            String resultado = produto.getResultado();
+
+            totalPrecoCusto += precoCusto;
+            totalPrecoVenda += precoVenda;
+
+            Prompt.imprimir("Resultado para o produto " + (i + 1) + ": " + resultado);
+        }
+
+        double mediaPrecoCusto = totalPrecoCusto / numProdutos;
+        double mediaPrecoVenda = totalPrecoVenda / numProdutos;
+
+        Prompt.imprimir("Média do preço de custo: " + mediaPrecoCusto);
+        Prompt.imprimir("Média do preço de venda: " + mediaPrecoVenda);
+    }
 }
--- br/edu/up/modelos/Produto.java
+++ br/edu/up/modelos/Produto.java
@@ -1,5 +1,33 @@
 package br.edu.up.modelos;
 
 public class Produto {
+    private double precoCusto;
+    private double precoVenda;
 
+    public Produto(double precoCusto, double precoVenda) {
+        this.precoCusto = precoCusto;
+        this.precoVenda = precoVenda;
+    }
+
+    private String calcularLucroPrejuizo() {
+        if (precoVenda > precoCusto) {
+            return "Lucro";
+        } else if (precoVenda < precoCusto) {
+            return "Prejuízo";
+        } else {
+            return "Empate";
+        }
+    }
+
+    public String getResultado() {
+        return calcularLucroPrejuizo();
+    }
+
+    public double getPrecoCusto() {
+        return precoCusto;
+    }
+
+    public double getPrecoVenda() {
+        return precoVenda;
+    }
 }
